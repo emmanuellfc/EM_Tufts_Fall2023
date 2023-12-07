1840727eddf9f88519926865471dce89d66558f2
JEFC <jflore10@tufts.edu>
2023-12-06 21:03:13 -0800

    - Work in Progress.

---
+++ jitfailure-dolfin_expression_d83a9c80dfc62476368b7dc5b385f57e/dolfin_expression_d83a9c80dfc62476368b7dc5b385f57e.cpp
@@ -0,0 +1,100 @@
+
+// Based on https://gcc.gnu.org/wiki/Visibility
+#if defined _WIN32 || defined __CYGWIN__
+    #ifdef __GNUC__
+        #define DLL_EXPORT __attribute__ ((dllexport))
+    #else
+        #define DLL_EXPORT __declspec(dllexport)
+    #endif
+#else
+    #define DLL_EXPORT __attribute__ ((visibility ("default")))
+#endif
+
+#include <dolfin/function/Expression.h>
+#include <dolfin/math/basic.h>
+#include <Eigen/Dense>
+
+
+// cmath functions
+using std::cos;
+using std::sin;
+using std::tan;
+using std::acos;
+using std::asin;
+using std::atan;
+using std::atan2;
+using std::cosh;
+using std::sinh;
+using std::tanh;
+using std::exp;
+using std::frexp;
+using std::ldexp;
+using std::log;
+using std::log10;
+using std::modf;
+using std::pow;
+using std::sqrt;
+using std::ceil;
+using std::fabs;
+using std::floor;
+using std::fmod;
+using std::max;
+using std::min;
+
+const double pi = DOLFIN_PI;
+
+
+namespace dolfin
+{
+  class dolfin_expression_d83a9c80dfc62476368b7dc5b385f57e : public Expression
+  {
+     public:
+       double kappa;
+double t;
+double k;
+
+
+       dolfin_expression_d83a9c80dfc62476368b7dc5b385f57e()
+       {
+            
+       }
+
+       void eval(Eigen::Ref<Eigen::VectorXd> values, Eigen::Ref<const Eigen::VectorXd> x) const override
+       {
+          values[0] = log(-kappa*pow(pi, 2)*t)*sin(pi*k*x[0]);
+
+       }
+
+       void set_property(std::string name, double _value) override
+       {
+          if (name == "kappa") { kappa = _value; return; }          if (name == "t") { t = _value; return; }          if (name == "k") { k = _value; return; }
+       throw std::runtime_error("No such property");
+       }
+
+       double get_property(std::string name) const override
+       {
+          if (name == "kappa") return kappa;          if (name == "t") return t;          if (name == "k") return k;
+       throw std::runtime_error("No such property");
+       return 0.0;
+       }
+
+       void set_generic_function(std::string name, std::shared_ptr<dolfin::GenericFunction> _value) override
+       {
+
+       throw std::runtime_error("No such property");
+       }
+
+       std::shared_ptr<dolfin::GenericFunction> get_generic_function(std::string name) const override
+       {
+
+       throw std::runtime_error("No such property");
+       }
+
+  };
+}
+
+extern "C" DLL_EXPORT dolfin::Expression * create_dolfin_expression_d83a9c80dfc62476368b7dc5b385f57e()
+{
+  return new dolfin::dolfin_expression_d83a9c80dfc62476368b7dc5b385f57e;
+}
+
